e9e5d41cc92f3f45268d52d18bfa6d404b2204bd
valter100 <va6648li-s@student.lu.se>
2023-11-24 16:44:27 +0100

    base

---
+++ Source/Aura/Private/AuraCharacterBase.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AuraCharacterBase.h"
+
+// Sets default values
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
