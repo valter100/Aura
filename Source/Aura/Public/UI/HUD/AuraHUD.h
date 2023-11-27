c489b1dfc32aed86c75897267534853882b50739
valter100 <va6648li-s@student.lu.se>
2023-11-27 20:08:09 +0100

    UI Overlay

---
+++ Source/Aura/Public/UI/HUD/AuraHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "AuraHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
